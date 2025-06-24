7acc319fb2e8735b1aa0a497885fd1bd230d2582
Udaymodak97 <warninglast369@gmail.com>
2025-06-24 12:08:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import telebot
 import requests
 
 # Replace this with your actual bot token from @BotFather
-BOT_TOKEN = 'os.getenv("BOT_TOKEN")'
+BOT_TOKEN = 'os.getenv("BOT_TOKEN )'
 bot = telebot.TeleBot(BOT_TOKEN)
 
 API_URL = "https://terabox-api.vercel.app/api?link="
